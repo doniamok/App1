0198c9489ee25ef456ce22e08c93684b5ee23500
doniamok <donia99200@gmail.com>
2021-11-03 18:20:08 +0200

    bmi

---
+++ app/src/main/java/com/doniamokalbaraqouni/app1/MainActivity.java
@@ -0,0 +1,26 @@
+package com.doniamokalbaraqouni.app1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE) ;
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void button2(View v){
+        Intent x = new Intent(MainActivity.this, login.class) ;
+        startActivity(x) ;
+    }
+}
---
+++ app/src/main/java/com/doniamokalbaraqouni/app1/login.java
@@ -0,0 +1,22 @@
+package com.doniamokalbaraqouni.app1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void button10(View v) {
+        Intent y = new Intent(login.this, registration.class);
+        startActivity(y);
+
+    }
+}
